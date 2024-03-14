fda95e71159ba3c18bba5acd1dfe1c8921e816e1
thehassanbilal <hassanbilalchannel@gmail.com>
2024-03-14 10:56:19 +0400

    adding  constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,10 @@
 export const BACKEND_URL = "http://209.38.234.189:3030";
+
+export const validFileTypes = [
+  "image/jpeg",
+  "image/png",
+  "image/gif",
+  "video/mp4",
+  "video/quicktime",
+  "video/mpeg",
+];
